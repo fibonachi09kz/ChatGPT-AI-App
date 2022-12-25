1498d3defc7c896ead1f9d92cde279439281fdcb
Danila Aleksin <danya09kz@gmail.com>
2022-12-26 05:13:38 +0600

    update design

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
 const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+const chatContainer = document.querySelector('#chat')
 
 let loadInterval
 
@@ -15,14 +15,14 @@ function loader(element) {
 
         // If the loading indicator has reached three dots, reset it
         if (element.textContent === '....') {
-            element.textContent = '';
+            element.textContent = '.';
         }
     }, 300);
 }
 
 function typeText(element, text) {
     let index = 0
-
+    
     let interval = setInterval(() => {
         if (index < text.length) {
             element.innerHTML += text.charAt(index)
@@ -31,6 +31,7 @@ function typeText(element, text) {
             clearInterval(interval)
         }
     }, 20)
+    
 }
 
 // generate unique ID for each message div of bot
@@ -45,20 +46,43 @@ function generateUniqueId() {
 }
 
 function chatStripe(isAi, value, uniqueId) {
-    return (
-        `
-        <div class="wrapper ${isAi && 'ai'}">
-            <div class="chat">
-                <div class="profile">
-                    <img
-                      src=${isAi ? bot : user}
-                      alt="${isAi ? 'bot' : 'user'}"
-                    />
-                </div>
-                <div class="message" id=${uniqueId}>${value}</div>
-            </div>
+    let today  = new Date();
+    let userSvg = `
+        <div class="w-10 h-10 flex items-center justify-center rounded-full bg-gray-700">
+             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-gray-300">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+             </svg>
+        </div>
+       
+    `
+    let botSvg = `
+        <div class="w-10 h-10 flex items-center justify-center rounded-full bg-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-gray-300">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+            </svg>
         </div>
     `
+    return (
+        `
+            <li class="${isAi && 'ai'}">
+                <div class="flex space-x-3">
+                    <div class="flex-shrink-0">
+                        ${isAi ? botSvg : userSvg}
+                    </div>
+                    <div>
+                        <div class="text-sm">
+                            <a href="#" class="font-medium text-gray-200">${isAi ? 'AI' : 'You'}</a>
+                        </div>
+                        <div class="mt-1 text-sm text-gray-400">
+                            <p class="message" id=${uniqueId}>${value}</p>
+                        </div>
+                        <div class="mt-2 text-sm space-x-2">
+                            <span class="text-gray-500 font-medium">${today.toLocaleString()}</span>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        `
     )
 }
 
@@ -66,7 +90,6 @@ const handleSubmit = async (e) => {
     e.preventDefault()
 
     const data = new FormData(form)
-
     // user's chatstripe
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
@@ -75,7 +98,7 @@ const handleSubmit = async (e) => {
 
     // bot's chatstripe
     const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+    chatContainer.innerHTML += chatStripe(true, "", uniqueId)
 
     // to focus scroll to the bottom
     chatContainer.scrollTop = chatContainer.scrollHeight;
@@ -97,12 +120,12 @@ const handleSubmit = async (e) => {
     })
 
     clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
+    messageDiv.innerHTML = ""
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n'
-
+        const parsedData = data.bot.trim()
+        
         typeText(messageDiv, parsedData)
     } else {
         const err = await response.text()
